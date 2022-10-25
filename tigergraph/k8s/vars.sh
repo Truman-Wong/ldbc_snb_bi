83f576a8498407d3c49d6f6eedc8f6cbae39eea6
truman-wang <truman.wang@tigergraph.com>
2022-10-25 04:57:28 +0000

    fix schema and checked

--- tigergraph/k8s/vars.sh
+++ tigergraph/k8s/vars.sh
@@ -11,8 +11,8 @@ export NUM_NODES=2 # number of pods or nodes
 export SF=100 # data source 100, 1000, 3000, 10000 ...
 export DOWNLOAD_THREAD=5 # number of download threads
 
-export TG_DATA_DIR=$HOME/tigergraph/data/sf${SF}
-export TG_PARAMETER=$HOME/tigergraph/data/parameters-sf${SF}
+export TG_DATA_DIR=$HOME/sf${SF}
+export TG_PARAMETER=$HOME/parameters-sf${SF}
 
 export DDL_PATH=`pwd`/ddl
 export QUERY_PATH=`pwd`/queries
